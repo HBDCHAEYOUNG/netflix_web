75b29f0b06b695f695cb2ecf9eeae149e60ccc81
HBDCHAEYOUNG <devyoon02@gmail.com>
2024-11-19 18:10:01 +0900

    환경설정 색이름

--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -1,5 +1,3 @@
-import ButtonMoreInfo from '@ui/button/button-moreinfo'
-import ButtonPlay from '@ui/button/button-play'
 import { Link } from 'react-router-dom'
 
 function Home() {
@@ -7,8 +5,6 @@ function Home() {
 		<div>
 			홈입니다
 			<Link to="/auth">로그인</Link>
-			<ButtonPlay />
-			<ButtonMoreInfo />
 		</div>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,52 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'primary/black': '#141414',
-				'primary/red': 'rgb(229, 9, 20)',
-				'primary/white': '#ffffff',
+				'Primary/Black': '#000000',
+				'Primary/Red': 'rgb(229, 9, 20)',
+				'Primary/White': '#ffffff',
 
-				'secondary/red-200': '#C11119',
-				'secondary/red-300': '#F50723', //동영상 재생 바
-				'secondary/blue-100': '#0071EB',
-				'secondary/green': '#46D369', //new
+				'Secondary/Red-100': '#EB3942',
+				'Secondary/Red-200': '#C11119',
+				'Secondary/Red-300': '#F50723', //동영상 재생 바
+				'Secondary/Blue-100': '#0071EB',
+				'Secondary/Blue-200': '#448EF4',
+				'Secondary/Blue-300': '#54B9C5',
+				'Secondary/Green': '#46D369', //new
 
-				'grey/grey-10': '#E5E5E5', //메뉴 글자색
-				'grey/grey-50': '#BCBCBC', //input text, 650 text
-				'grey/grey-100': '#B3B3B3', //footer text
-				'grey/grey-200': '#808080', //border
-				'grey/grey-600': '#333333', //선택된 에피소드 bg
-				'grey/grey-650': '#2F2F2F', //프로그램 설명 bg
-				'grey/grey-700': '#2A2A2A', //완전 큰 선, 큰 글자 배경
+				'Grey/Grey-10': '#E5E5E5', //메뉴 글자색
+				'Grey/Grey-20': '#DCDCDC',
+				'Grey/Grey-25': '#D2D2D2',
+				'Grey/Grey-25': '#BCBCBC', //input text, 650 text
+				'Grey/Grey-100': '#B3B3B3', //footer text
+				'Grey/Grey-150': '#979797',
+				'Grey/Grey-200': '#808080', //border
+				'Grey/Grey-250': '#777777',
+				'Grey/Grey-300T40': 'rgb(109, 109, 110, 40%)',
+				'Grey/Grey-300T70': 'rgb(109, 109, 110, 70%)',
+				'Grey/Grey-350': '#545454',
+				'Grey/Grey-400': '#414141',
+				'Grey/Grey-450': '#404040',
+				'Grey/Grey-500': '#3A3A3A',
+				'Grey/Grey-550': '#363636',
+				'Grey/Grey-600T60': 'rgb(51, 51, 51, 60%)',
+				'Grey/Grey-600': '#333333', //선택된 에피소드 bg
+				'Grey/Grey-650': '#2F2F2F', //프로그램 설명 bg
+				'Grey/Grey-700': '#2A2A2A', //완전 큰 선, 큰 글자 배경
+				'Grey/Grey-750': '#262626',
+				'Grey/Grey-800': '#232323',
+				'Grey/Grey-850': '#181818',
+				'Grey/Grey-900': '#141414',
 
-				'primary/red-02': '#831010',
+				'Transparent White/15%': 'rgb(255, 255, 255, 15%)',
+				'Transparent White/20%': 'rgb(255, 255, 255, 20%)',
+				'Transparent White/30%': 'rgb(255, 255, 255, 30%)',
+				'Transparent White/35%': 'rgb(255, 255, 255, 35%)',
+				'Transparent White/50%': 'rgb(255, 255, 255, 50%)',
+				'Transparent White/70%': 'rgb(255, 255, 255, 70%)',
+
+				'TransparentBlack/30%': 'rgb(0, 0, 0, 30%)',
+				'TransparentBlack/60%': 'rgb(0, 0, 0, 60%)',
+				'TransparentBlack/60%': 'rgb(0, 0, 0, 90%)',
 			},
 		},
 	},
@@ -69,6 +97,7 @@ export default {
 					'-ms-overflow-style': 'none',
 					'scrollbar-width': 'none',
 				},
+
 				//typography
 				'.regular-caption2': {
 					fontFamily: 'Netflix Sans',
