60efd45fea8dd1b6c4cb03edaaa56bf2f9d88d4a
HBDCHAEYOUNG <devyoon02@gmail.com>
2024-12-12 18:01:33 +0900

    header search update

--- src/app/layouts/header.tsx
+++ src/app/layouts/header.tsx
@@ -2,59 +2,99 @@ import Button from '@ui/button/button'
 import { Link } from 'react-router-dom'
 import netflixLogo from '@images/logo.png'
 import { AuthStore } from '@store/auth-store'
-import { Menu, MenuBell } from '@ui/index'
+import { InputText, Menu, MenuBell } from '@ui/index'
 import PencilIcon from '@icons/pencil.svg?react'
 import TransferIcon from '@icons/account.svg?react'
 import PersonIcon from '@icons/person.svg?react'
 import ServiceIcon from '@icons/question.svg?react'
+import SearchIcon from '@icons/search.svg?react'
 import post from '@images/post.png'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import Form from '@ui/form/form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export function Header() {
+	const [isSearch, setIsSearch] = useState(false)
+
+	const searchSchema = z.object({
+		search: z
+			.string()
+			.min(1)
+			.regex(/^[a-zA-Z0-9\s]*$/, '특수문자는 입력할 수 없습니다.'),
+	})
+	const form = useForm<z.infer<typeof searchSchema>>({
+		resolver: zodResolver(searchSchema),
+	})
+
 	const { isLogin, setLogin, setLogout } = AuthStore()
 
-	console.log(new Date().toISOString())
-	return (
-		<header className="fixed left-1/2 z-10 flex w-full -translate-x-1/2 items-center justify-between py-6 max-width-desktop">
-			<Link to="/">
-				<img src={netflixLogo} alt="netflix" className="cursor-pointer" />
-			</Link>
+	const handleSubmit = () => {
+		console.log(111)
+	}
 
-			{isLogin ? (
-				<div className="flex items-center gap-4">
-					<MenuBell
-						items={[
-							{ image: post, title: 'New Content', description: 'Watch The Garfield Movie | Netflix', at: '2024-12-10T07:10:27.951Z' },
-							{ image: post, title: 'New Content', description: 'Watch The Garfield Movie | Netflix', at: '2024-12-10T07:10:27.951Z' },
-							{ image: post, title: 'New Content', description: 'Watch The Garfield Movie | Netflix', at: '2024-12-10T07:10:27.951Z' },
-						]}
-					/>
-					<Menu
-						label="Profile"
-						items={[
-							{
-								icon: <PencilIcon />,
-								title: 'Manage Your Profile',
-							},
-							{
-								icon: <TransferIcon />,
-								title: 'Profile Transfer',
-							},
-							{
-								title: 'Account',
-								icon: <PersonIcon />,
-							},
-							{ title: 'Customer Service Center', icon: <ServiceIcon /> },
-							{ title: 'Log out Of Netflix', onClick: setLogout, className: 'mt-2 justify-center border-t border-Grey/Grey-200 pr-0' },
-						]}
-					/>
-				</div>
-			) : (
-				<Link to="/auth/login">
-					<Button className="h-[32px] w-[77px]" onClick={setLogin}>
-						Login
-					</Button>
+	return (
+		<header className="fixed left-1/2 z-10 flex w-full -translate-x-1/2 flex-col items-center justify-between py-6 max-width-desktop">
+			<div className="flex w-full justify-between">
+				<Link to="/">
+					<img src={netflixLogo} alt="netflix" className="cursor-pointer" />
 				</Link>
-			)}
+
+				{isLogin ? (
+					<Form onSubmit={handleSubmit} form={form} className="flex items-center gap-4">
+						{isSearch ? (
+							<Form.Item
+								name="search"
+								renderItem={(field) => (
+									<InputText
+										name="search"
+										icon={<SearchIcon />}
+										className="h-[38px] w-[250px] rounded-none !outline-none"
+										defaultFocused={true}
+										{...field}
+									/>
+								)}
+							/>
+						) : (
+							<SearchIcon className="cursor-pointer" onClick={() => setIsSearch(true)} />
+						)}
+						<MenuBell
+							items={[
+								{ image: post, title: 'New Content', description: 'Watch The Garfield Movie | Netflix', at: '2024-12-10T07:10:27.951Z' },
+								{ image: post, title: 'New Content', description: 'Watch The Garfield Movie | Netflix', at: '2024-12-10T07:10:27.951Z' },
+								{ image: post, title: 'New Content', description: 'Watch The Garfield Movie | Netflix', at: '2024-12-10T07:10:27.951Z' },
+							]}
+						/>
+						<Menu
+							label="Profile"
+							items={[
+								{
+									icon: <PencilIcon />,
+									title: 'Manage Your Profile',
+								},
+								{
+									icon: <TransferIcon />,
+									title: 'Profile Transfer',
+								},
+								{
+									title: 'Account',
+									icon: <PersonIcon />,
+								},
+								{ title: 'Customer Service Center', icon: <ServiceIcon /> },
+								{ title: 'Log out Of Netflix', onClick: setLogout, className: 'mt-2 justify-center border-t border-Grey/Grey-200 pr-0' },
+							]}
+						/>
+					</Form>
+				) : (
+					<Link to="/auth/login">
+						<Button className="h-[32px] w-[77px]" onClick={setLogin}>
+							Login
+						</Button>
+					</Link>
+				)}
+			</div>
+			<div className="h-screen w-full" onClick={() => setIsSearch(false)} />
 		</header>
 	)
 }
--- src/shared/ui/input/input-text.tsx
+++ src/shared/ui/input/input-text.tsx
@@ -5,55 +5,62 @@ import { cn } from '@lib/utils'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 	label?: string
+	icon?: React.ReactNode
+	defaultFocused?: boolean
 }
 
-export const InputText = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, label, ...props }, ref) => {
-	const [isFocused, setIsFocused] = React.useState(false)
-	const inputRef = useRef<HTMLDivElement>(null)
+export const InputText = React.forwardRef<HTMLInputElement, InputProps>(
+	({ className, type, label, icon, defaultFocused = false, ...props }, ref) => {
+		const [isFocused, setIsFocused] = React.useState(defaultFocused)
+		const inputRef = useRef<HTMLDivElement>(null)
 
-	useEffect(() => {
-		const handleClickOutside = (event: MouseEvent) => {
-			if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
-				const inputElement = inputRef.current.querySelector('input')
-				if (!inputElement?.value) {
-					setIsFocused(false)
+		useEffect(() => {
+			const handleClickOutside = (event: MouseEvent) => {
+				if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
+					const inputElement = inputRef.current.querySelector('input')
+					if (!inputElement?.value) {
+						setIsFocused(false)
+					}
 				}
 			}
-		}
 
-		document.addEventListener('mousedown', handleClickOutside)
-		return () => {
-			document.removeEventListener('mousedown', handleClickOutside)
-		}
-	}, [])
+			document.addEventListener('mousedown', handleClickOutside)
+			return () => {
+				document.removeEventListener('mousedown', handleClickOutside)
+			}
+		}, [])
 
-	return (
-		<div ref={inputRef}>
-			{isFocused ? (
-				<div
-					className={cn(
-						'flex h-14 w-full flex-col justify-center rounded-md border border-[#808080] bg-Primary/Black px-3 text-Grey/Grey-50 outline outline-Primary/White Medium-Caption2',
-						className,
-					)}
-				>
-					<label className="text-left text-xs text-Grey/Grey-50">{label}</label>
-					<input className={cn('bg-transparent Medium-Body focus:outline-none')} type={type} ref={ref} {...props} />
-				</div>
-			) : (
-				<input
-					placeholder={label}
-					type={type}
-					onFocus={() => setIsFocused(true)}
-					className={cn(
-						'h-14 w-full rounded-md border border-[#808080] bg-Primary/Black px-3 text-Grey/Grey-50 focus:outline-none',
-						className,
-					)}
-					ref={ref}
-					{...props}
-				/>
-			)}
-		</div>
-	)
-})
+		return (
+			<div ref={inputRef}>
+				{isFocused ? (
+					<div
+						className={cn(
+							'flex h-14 w-full flex-col justify-center rounded-md border border-[#808080] bg-Primary/Black px-3 text-Grey/Grey-50 outline outline-Primary/White Medium-Caption2',
+							className,
+						)}
+					>
+						<label className="text-left text-xs text-Grey/Grey-50">{label}</label>
+						<div className="flex items-center gap-2">
+							{icon && <span className="flex items-center">{icon}</span>}
+							<input className={cn('w-full bg-transparent Medium-Body focus:outline-none')} type={type} ref={ref} {...props} />
+						</div>
+					</div>
+				) : (
+					<input
+						placeholder={label}
+						type={type}
+						onFocus={() => setIsFocused(true)}
+						className={cn(
+							'h-14 w-full rounded-md border border-[#808080] bg-Primary/Black px-3 text-Grey/Grey-50 focus:outline-none',
+							className,
+						)}
+						ref={ref}
+						{...props}
+					/>
+				)}
+			</div>
+		)
+	},
+)
 
 InputText.displayName = 'InputText'
