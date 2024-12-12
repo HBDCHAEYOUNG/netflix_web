8968162bf68f3a2b7c580347ac63149584b37c9e
HBDCHAEYOUNG <devyoon02@gmail.com>
2024-12-12 22:53:22 +0900

    헤더 업데이트

--- src/app/layouts/header.tsx
+++ src/app/layouts/header.tsx
@@ -1,35 +1,24 @@
-import Button from '@ui/button/button'
-import { Link } from 'react-router-dom'
-import netflixLogo from '@images/logo.png'
-import { AuthStore } from '@store/auth-store'
-import { InputText, Menu, MenuBell } from '@ui/index'
-import PencilIcon from '@icons/pencil.svg?react'
 import TransferIcon from '@icons/account.svg?react'
+import PencilIcon from '@icons/pencil.svg?react'
 import PersonIcon from '@icons/person.svg?react'
 import ServiceIcon from '@icons/question.svg?react'
 import SearchIcon from '@icons/search.svg?react'
+import netflixLogo from '@images/logo.png'
 import post from '@images/post.png'
+import { AuthStore } from '@store/auth-store'
+import Button from '@ui/button/button'
+import Form from '@ui/form/form'
+import { InputText, Menu, MenuBell } from '@ui/index'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import Form from '@ui/form/form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { Link } from 'react-router-dom'
 
 export function Header() {
 	const [isSearch, setIsSearch] = useState(false)
 
-	const searchSchema = z.object({
-		search: z
-			.string()
-			.min(1)
-			.regex(/^[a-zA-Z0-9\s]*$/, '특수문자는 입력할 수 없습니다.'),
-	})
-	const form = useForm<z.infer<typeof searchSchema>>({
-		resolver: zodResolver(searchSchema),
-	})
+	const form = useForm()
 
 	const { isLogin, setLogin, setLogout } = AuthStore()
-
 	const handleSubmit = () => {
 		console.log(111)
 	}
@@ -42,23 +31,36 @@ export function Header() {
 				</Link>
 
 				{isLogin ? (
-					<Form onSubmit={handleSubmit} form={form} className="flex items-center gap-4">
-						{isSearch ? (
-							<Form.Item
-								name="search"
-								renderItem={(field) => (
-									<InputText
-										name="search"
-										icon={<SearchIcon />}
-										className="h-[38px] w-[250px] rounded-none !outline-none"
-										defaultFocused={true}
-										{...field}
-									/>
-								)}
-							/>
-						) : (
-							<SearchIcon className="cursor-pointer" onClick={() => setIsSearch(true)} />
-						)}
+					<div className="ml-auto flex w-full items-center gap-4">
+						{[
+							{ title: 'home', path: '/' },
+							{ title: 'seiries', path: '/tv' },
+							{ title: 'movie', path: '/movies' },
+							{ title: 'New!The latest tranding content', path: 'latest' },
+							{ title: 'My List', path: 'my-list' },
+						].map((item, index) => (
+							<Link to={item.path} key={index} className="min-w-max Regular-Smallbody">
+								{item.title}
+							</Link>
+						))}
+						<Form onSubmit={handleSubmit} form={form} className="flex w-full items-center gap-4">
+							{isSearch ? (
+								<Form.Item
+									name="search"
+									renderItem={(field) => (
+										<InputText
+											name="search"
+											icon={<SearchIcon />}
+											className="h-[38px] w-[250px] rounded-none !border-Primary/White !outline-none"
+											defaultFocused={true}
+											{...field}
+										/>
+									)}
+								/>
+							) : (
+								<SearchIcon className="ml-auto cursor-pointer" onClick={() => setIsSearch(true)} />
+							)}
+						</Form>
 						<MenuBell
 							items={[
 								{ image: post, title: 'New Content', description: 'Watch The Garfield Movie | Netflix', at: '2024-12-10T07:10:27.951Z' },
@@ -85,7 +87,7 @@ export function Header() {
 								{ title: 'Log out Of Netflix', onClick: setLogout, className: 'mt-2 justify-center border-t border-Grey/Grey-200 pr-0' },
 							]}
 						/>
-					</Form>
+					</div>
 				) : (
 					<Link to="/auth/login">
 						<Button className="h-[32px] w-[77px]" onClick={setLogin}>
