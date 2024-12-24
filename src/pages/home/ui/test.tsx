a6a9dde02582274d55693bf7549cf42c3a3e6fa1
HBDCHAEYOUNG <devyoon02@gmail.com>
2024-12-24 21:48:37 +0900

    디테일 업데이트

--- src/pages/home/ui/test.tsx
+++ src/pages/home/ui/test.tsx
@@ -1,7 +1,10 @@
 import post from '@images/movie.png'
-import title from '@images/movie-title.png'
+import title from '@images/movie-title-small.png'
+
 import Button from '@ui/button/button'
 import Play from '@icons/play.svg?react'
+import TvMa from '@icons/tv-ma.svg?react'
+import Hd from '@icons/hd.svg?react'
 const movie = {
 	image: post,
 	title: title,
@@ -9,22 +12,57 @@ const movie = {
 		'The Super Mario Bros. Movie is an upcoming American computer-animated action-adventure comedy film produced by Illumination and distributed by Universal Pictures.',
 	duration: '1 hours 30 minutes',
 	genre: 'Action, Adventure, Comedy',
+	cast: 'Chris Pratt, Anya Taylor-Joy, Charlie Day, Jack Black',
+	etc: 'Exciting, Thrilling, Heartwarming, Family-friendly',
 	releaseDate: '2023-04-05',
-	rating: '19',
+	rating: 'tv-ma',
+	season: 3,
+	videoQuality: 'hd',
 }
 
 export function Test() {
 	return (
 		<div>
-			<div className="w-[850px]">
-				<img src={movie.image} alt={movie.title} className="relative left-0 top-0 w-full" />
+			<div className="relative w-[850px] bg-Grey/Grey-850">
+				<img
+					src={movie.image}
+					alt={movie.title}
+					className="mask-linear-gradient absolute left-0 top-0 z-0 w-full"
+					style={{
+						maskImage: 'linear-gradient(to bottom, black, transparent)',
+						WebkitMaskImage: 'linear-gradient(to bottom, black, transparent)',
+					}}
+				/>
 
-				<div>
+				<div className="relative h-[480px] w-[850px] place-content-end px-12 pb-16">
 					<img src={movie.title} alt={movie.title} />
-					<Button theme="white" className="gap2 h-[43px] w-[119px] flex-center Medium-Body">
-						<Play className="[&>path]:fill-black" />
-						Play
-					</Button>
+
+					<nav>
+						<Button theme="white" className="mt-6 h-[43px] w-[119px] gap-2 flex-center Medium-Body">
+							<Play className="[&>path]:fill-black" />
+							Play
+						</Button>
+						{/* 아이콘 추가 */}
+					</nav>
+				</div>
+
+				<div className="px-12">
+					<div className="flex">
+						<div>
+							<p className="flex items-center gap-2 text-Grey/Grey-50">
+								{movie.season} Seasons {movie.releaseDate.slice(0, 4)}
+								{movie.videoQuality === 'hd' && <Hd />}
+							</p>
+							{movie.rating === 'tv-ma' && <TvMa />}
+							<span className="mt-6 block Medium-Body">{movie.description}</span>
+						</div>
+
+						<p>
+							cast : {movie.cast}
+							genre : {movie.genre}
+							etc : {movie.etc}
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
