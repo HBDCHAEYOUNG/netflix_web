f88f64c9ae9761c18638946a93322e74466431fb
HBDCHAEYOUNG <devyoon02@gmail.com>
2025-01-22 12:12:01 +0900

    영상 재생 페이지 수정

--- src/app/layouts/play-header.tsx
+++ src/app/layouts/play-header.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 export function PlayHeader() {
 	const navigate = useNavigate()
 	return (
-		<div className="fixed left-0 top-0 z-10 p-9">
+		<div className="fixed left-0 top-0 z-10 px-12 py-9">
 			<ArrowBackIcon className="h-9 w-9 cursor-pointer" onClick={() => navigate(-1)} />
 		</div>
 	)
--- src/widgets/video/ui/video.tsx
+++ src/widgets/video/ui/video.tsx
@@ -1,7 +1,7 @@
 export function Video() {
 	return (
-		<div>
-			<video controls autoPlay>
+		<div className="h-screen">
+			<video controls autoPlay className="mx-auto h-full object-contain">
 				<source src="https://videos.pexels.com/video-files/9116112/9116112-uhd_2560_1440_25fps.mp4" type="video/mp4" />
 			</video>
 		</div>
